7ece0ad4dae42e82b133bdf5be6fcf49e9a1c03e
Daniel-Stafford <stafford.daniel@gmail.com>
2019-08-21 14:52:16 +0300

    part 2c phonebook

--- part2/phonebook/phonebook/src/App.js
+++ part2/phonebook/phonebook/src/App.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Persons from './components/Persons';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [newFilter, setNewFilter] = useState('');
   const [newToDisplay, setToDisplay] = useState(persons);
+
+  useEffect(() => {
+    console.log('effect');
+    axios.get('http://localhost:3001/persons').then(response => {
+      console.log('promise fulfilled');
+      setPersons(response.data);
+    });
+  }, []);
+  console.log('render', persons.length, 'persons');
 
   let personsToShow = persons;
 
@@ -37,7 +42,7 @@ const App = () => {
           />
         </div>
       </form>
-      <h2>to Add</h2>
+      <h2>To Add</h2>
       <PersonForm
         newName={newName}
         setNewName={setNewName}
